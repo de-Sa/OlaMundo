51a5204c708d48b8dc30efa518ec6eddeb7d8642
de-Sa <lucas.desa10@outlook.com>
2024-03-10 09:17:05 -0300

    1.0.0

--- src/componentes/Banner/index.js
+++ src/componentes/Banner/index.js
@@ -5,27 +5,32 @@ import minhaFoto from "assets/minha_foto.png";
 export default function Banner() {
   return (
     <div className={styles.banner}>
-      <div className={styles.apresentacao}>
-        <h1 className={styles.titulo}>Olá, Mundo!</h1>
+      <div className={styles.container}>
+        <div className={styles.apresentacao}>
+          <h1 className={styles.titulo}>Olá, Mundo!</h1>
 
-        <p className={styles.paragrafo}>
-        Boas-vindas! Sou Lucas de Sá, Analista e Desenvolvedor de Software. Possuo a combinação ideal de habilidade e determinação para transformar suas ideias em realidade. Vamos conversar?
-        </p>
-      </div>
+          <p className={styles.paragrafo}>
+            Boas-vindas! Sou Lucas de Sá, Analista e Desenvolvedor de Software.
+            Possuo a combinação ideal de habilidade e determinação para
+            transformar suas ideias em realidade. Vamos conversar?
+          </p>
+        </div>
+        </div>
 
-      <div className={styles.imagens}>
-        <img
-          className={styles.circuloColorido}
-          src={circuloColorido}
-          aria-hidden={true}
-          alt="Complemento, Fundo da Foto"
-        />
+        <div className={styles.imagens}>
+          <img
+            className={styles.circuloColorido}
+            src={circuloColorido}
+            aria-hidden={true}
+            alt="Complemento, Fundo da Foto"
+          />
 
-        <img
-          className={styles.minhaFoto}
-          src={minhaFoto}
-          alt="Foto do Lucas"
-        />
+          <img
+            className={styles.minhaFoto}
+            src={minhaFoto}
+            alt="Foto do Lucas"
+          />
+        
       </div>
     </div>
   );
