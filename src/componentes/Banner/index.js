c9047056f6696cdbdfa5482b367998243c3a6c64
Lucas de Sá <119504411+de-Sa@users.noreply.github.com>
2025-02-15 12:20:32 -0300

    test

--- src/componentes/Banner/index.js
+++ src/componentes/Banner/index.js
@@ -1,5 +1,4 @@
 import styles from "./Banner.module.css";
-import circuloColorido from "assets/circulo_colorido.png";
 import minhaFoto from "assets/minha_foto.png";
 
 export default function Banner() {
@@ -10,7 +9,7 @@ export default function Banner() {
           <h1 className={styles.titulo}>Olá, Mundo!</h1>
 
           <p className={styles.paragrafo}>
-            Boas-vindas! Sou Lucas de Sá, Analista e Desenvolvedor de Software.
+            Boas-vindas! Sou Lucas de Sá, Desenvolvedor Web e futuro full Stack.
             Possuo a combinação ideal de habilidade e determinação para
             transformar suas ideias em realidade. Vamos conversar?
           </p>
@@ -18,13 +17,7 @@ export default function Banner() {
         </div>
 
         <div className={styles.imagens}>
-          <img
-            className={styles.circuloColorido}
-            src={circuloColorido}
-            aria-hidden={true}
-            alt="Complemento, Fundo da Foto"
-          />
-
+    
           <img
             className={styles.minhaFoto}
             src={minhaFoto}
